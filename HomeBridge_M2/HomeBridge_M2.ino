1709b0050395701db5b2aeed831c0ab82691692f
keviniporre <keviniporres@gmail.com>
2023-11-20 22:52:43 -0400

    Update HomeBridge_M2.ino
    Updated Pin number for devices and added comments.

--- HomeBridge_M2/HomeBridge_M2.ino
+++ HomeBridge_M2/HomeBridge_M2.ino
@@ -4,17 +4,24 @@
 #include "DEV_Sensors.h"
 #include "DEV_Relay.h"
 
-//PIN CONFIGURATION
-int controlPin = 13;
-int motionPin = 16;
-int relayPin1 = 22; //changed from 19
-int relayPin2 = 18;
-int relayPin3 = 19;
-int relayPin4 = 21;
-int touchPin1 = 26;
-int touchPin2 = 27;
-int touchPin3 = 34;//changed from 32
-int touchPin4 = 35;
+// PIN CONFIGURATION
+int controlPin = 13;  // reset wifi settings
+// SENSORS INPUT SIGNAL PIN
+int contactPin = 27;  // main door
+int motionPin = 26;   // main door
+int dhtPin = 4;       // main control
+// RELAYS OUTPUT SIGNAL PIN
+int relayPin1 = 16;   // main light
+int relayPin2 = 17;   // second light
+int relayPin3 = 18;   // outside light
+int relayPin4 = 19;   // bar light
+int relayPin5 = 21;   // TV light
+// TOUCH INPUT SIGNAL PIN
+int touchPin1 = 25;   // main light
+int touchPin2 = 23;   // second light
+int touchPin3 = 32;   // outside light
+int touchPin4 = 35;   // bar light
+int touchPin5 = 34;   // TV light
 
 void setup() {
   // put your setup code here, to run once:
